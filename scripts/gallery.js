84caf96783e5c69085a5b683672f4789852a4e49
kwuffa <mirevg06@gmail.com>
2023-06-17 21:49:11 +0300

    gallery buttons

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,10 +1,20 @@
 let buttons = Array.from(document.querySelectorAll(".slide"));
-function slideNext(){
+let prevBtn = document.querySelector(".gallery_prevBtn");
+let nextBtn = document.querySelector(".gallery_nextBtn");
+function slideNext(minus=false){
     let currentBlock = document.querySelector(".images_block_wrapper.active");
     let currentBtn = document.querySelector(".slide.active");
-    let dataAttr = Number(currentBlock.getAttribute("data-gallery"))+1;
-    if(dataAttr > 3){
-        dataAttr = 1;
+    let dataAttr;
+    if(!minus){
+        dataAttr = Number(currentBlock.getAttribute("data-gallery"))+1;
+        if(dataAttr > 3){
+            dataAttr = 1;
+        };
+    } else{
+        dataAttr = Number(currentBlock.getAttribute("data-gallery"))-1;
+        if(dataAttr < 1){
+            dataAttr = 3;
+        };
     };
     let newBlock = document.querySelector(`.images_block_wrapper[data-gallery="${dataAttr}"]`);
     let newBtn = document.querySelector(`.slide[data-gallery="${dataAttr}"]`);
@@ -35,4 +45,15 @@ buttons.forEach(element => {
             intervalId = setInterval(slideNext, 4000);
         }
     });
+});
+
+prevBtn.addEventListener("click", function(){
+    clearInterval(intervalId);
+    slideNext(true);
+    intervalId = setInterval(slideNext, 4000);
+});
+nextBtn.addEventListener("click", function(){
+    clearInterval(intervalId);
+    slideNext();
+    intervalId = setInterval(slideNext, 4000);
 });
